87c28b72eacd9da30ea6fcb9166c9f2a836306b5
Evan <ebeniezer@gmail.com>
2016-01-28 08:34:16 -0500

    Adding http request placeholder

---
+++ main.go
@@ -0,0 +1,9 @@
+// utility for determining single origin
+// placeholder for http request
+
+reader := strings.NewReader(`{"body":123}`)
+request, err := http.NewRequest("GET", "http://localhost:3030/foo", reader)
+// TODO: check err
+client := &http.Client{}
+resp, err := client.Do(request)
+// TODO: check err
