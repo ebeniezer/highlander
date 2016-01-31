76bccf1165fee672d234e3e110d6ce592e8fbe4b
ebeniezer <ebeniezer@gmail.com>
2016-01-31 18:25:59 -0500

    Adding main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 // utility for determining single origin
 // placeholder for http request
 
+package main
+
 reader := strings.NewReader(`{"body":123}`)
 request, err := http.NewRequest("GET", "http://localhost:3030/foo", reader)
 // TODO: check err
